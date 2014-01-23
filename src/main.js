868d3be5ae3655b156d79aa1a987737a4abfca55
Filipe Linhares <lip.linhares@gmail.com>
2014-01-23 21:45:09 -0200

    fix only input text

--- src/main.js
+++ src/main.js
@@ -1,22 +1,25 @@
 window.addEventListener('DOMContentLoaded', function(e) {
-	//Cria o elemento button
-	var btn = document.createElement('button');
-	//Coloca position absolute, pra n precisar se preocupar com nada
-	btn.style.position="absolute";
-	btn.style.top="0";
 
-	//Coloca um texto pra conseguirmos achar o botão
-	btn.appendChild(document.createTextNode('AutoPut'))
-	//Coloca o botão no body para ser usado
-	document.body.appendChild(btn);
+  //Cria o elemento button
+  var btn = document.createElement('button');
 
-	//Declara o evento, para que ele seja acionado quando clicar
-	btn.addEventListener('click', function (e) {
-		var inputs = document.getElementsByTagName('input');
-		var i = 0;
-		for(i;i < inputs.length;i++){
-			inputs[i].value = 'Test';
-		}
-		document.body.removeChild(btn);
-	});
+  //Coloca position absolute, pra n precisar se preocupar com nada
+  btn.style.position = "absolute";
+  btn.style.top = "0";
+
+  //Coloca um texto pra conseguirmos achar o botão
+  btn.appendChild(document.createTextNode('AutoPut'));
+
+  //Coloca o botão no body para ser usado
+  document.body.appendChild(btn);
+
+  //Declara o evento, para que ele seja acionado quando clicar
+  btn.addEventListener('click', function(e) {
+    var inputs = document.querySelectorAll('input[type="text"]');
+    var i = 0;
+    for (i; i < inputs.length; i++) {
+      inputs[i].value = 'Test';
+    }
+    document.body.removeChild(btn);
+  });
 });
