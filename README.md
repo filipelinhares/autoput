a88ecedb288fb3543915c46c0b8ae71cea7e70ba
Filipe Linhares <lip.linhares@gmail.com>
2014-07-06 15:46:11 -0300

    Remove old files

--- README.md
+++ README.md
@@ -1,6 +1,73 @@
-AutoPut
-====================
+# Autoput
+### Filling all spaces
+[DEMO](http://filipelinhares.github.io/autoput)
 
-##[Example](http://filipelinhares.github.io/autoput)
+> A simple inplementatio to help you a fill all inputs in your forms.
 
-Simple script for autocomplete input texts.
+## How use
+
+Download the `autoput.js` [here]() and add on bottom of your page.
+
+```
+    <script src="autoput.js"></script>
+<body>
+
+```
+
+When the page loads, Autoput automatically adds a button on top of the page to you use hin.
+
+## Customization
+
+If you don't like so much the default values of Autoput, don't worry, you can change this with the custom options.
+
+After load the `autoput.js` you can defined an value with each option.
+
+```
+<script src="autoput.js"></script>
+<script>
+    Autoput.custom({
+        text: 'Other value to input text',
+        tel: '(222) 123-1234'
+    });
+    Autoput.on();
+</script>
+```
+
+#### Atenction
+
+The values of `date`, `week` and others like this, you will need follow the pattern:
+
+- date = YYYY-MM-DD
+- datetime: YYYYMM-DDT00:00Z
+- week: YYYY-W00
+- month: YYYY-MM
+
+## Contribute
+
+You'll need:
+[Node](http://nodejs.org/) - [NPM](https://www.npmjs.org/) - [Gulp](http://gulpjs.com/) - [Dalekjs](http://dalekjs.com/)
+
+Fork the project.
+
+`git clone `
+
+Install all dependencies
+
+`npm install`
+
+#### Gulp commands
+
+`gulp watch`
+
+It's create a server in `localhost:8081`.
+
+`gulp`
+
+Minify, Jshint and put the result on `dist/`.
+
+#### Tests
+
+All tests resulst are in `report`.
+
+## License
+MIT
--- src/autoput.js
+++ src/autoput.js
@@ -1,25 +1,139 @@
-window.addEventListener('DOMContentLoaded', function(e) {
+var Autoput = (function() {
+  'use strict';
 
-  //# Create the button element.
-  var btn = document.createElement('button');
+  function merge(obj1, obj2) {
+    var obj3 = {};
+    for (var x in obj1) {
+      obj3[x] = obj1[x];
+    }
+    for (var y in obj2) {
+      obj3[y] = obj2[y];
+    }
 
-  //# Put css style to button.
-  btn.style.position = "absolute";
-  btn.style.top = "0";
+    return obj3;
+  }
 
-  //# Put a value into button.
-  btn.appendChild(document.createTextNode('AutoPut'));
+  function buttonCreate() {
+    var button = document.createElement('button');
+    button.style.position = 'absolute';
+    button.style.top = 0;
+    button.style.left = 0;
+    button.id = 'autoput-button';
+    button.textContent = 'Click to Autoput';
+    document.body.appendChild(button);
 
-  //# Append the button in body.
-  document.body.appendChild(btn);
+    return button;
+  }
 
-  //# When button is clicked insert 'Test' value.
-  btn.addEventListener('click', function(e) {
-    var inputs = document.querySelectorAll('input[type="text"]');
+  function originalOptions() {
+    var properts = {
+      text: 'Lorem ipsun dolor',
+      email: 'example@example.com',
+      date: '2045-12-07',
+      datetime: '2045-07-12T00:55',
+      week: '2045-W02',
+      month: '2045-12',
+      tel: '(555) 555-5555',
+      baseNumber: 10,
+      url: 'http://www.example.com',
+      search: 'Searching for Unicorns',
+      radio: true,
+      checkbox: true,
+      range: 100
+    };
+
+    return properts;
+  }
+
+  var custom;
+
+  function customOptions(values) {
+    custom = values;
+
+    /* test-code */
+    window.api_test = values;
+    /* end-test-code */
+  }
+
+
+  function whenClicked() {
+    var original = originalOptions(),
+      options = merge(original, custom),
+      inputs = document.querySelectorAll('input'),
+      area = document.querySelectorAll('textarea'),
+      opt = document.querySelectorAll('option');
+
+    //# Fill inputs from all type
     var i = 0;
     for (i; i < inputs.length; i++) {
-      inputs[i].value = 'Test';
+
+      switch (inputs[i].type) {
+        case 'text':
+          inputs[i].value = options.text;
+          break;
+        case 'email':
+          inputs[i].value = options.email;
+          break;
+        case 'date':
+          inputs[i].value = options.date;
+          break;
+        case 'datetime-local':
+          inputs[i].value = options.datetime;
+          break;
+        case 'week':
+          inputs[i].value = options.week;
+          break;
+        case 'month':
+          inputs[i].value = options.month;
+          break;
+        case 'tel':
+          inputs[i].value = options.tel;
+          break;
+        case 'number':
+          inputs[i].value = Math.floor(Math.random() * options.baseNumber + 1);
+          break;
+        case 'url':
+          inputs[i].value = options.url;
+          break;
+        case 'search':
+          inputs[i].value = options.search;
+          break;
+        case 'radio':
+          inputs[i].setAttribute('checked', options.radio);
+          break;
+        case 'checkbox':
+          inputs[i].setAttribute('checked', options.checkbox);
+          break;
+        case 'range':
+          inputs[i].value = Math.floor(Math.random() * options.range + 1);
+          break;
+      }
     }
-    document.body.removeChild(btn);
-  });
-});
+
+    //# Fill textarea
+    var x = 0;
+    for (x; x < area.length; x++) {
+      area[x].value = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.';
+    }
+
+    //# Fill select box
+    var y = 0;
+    for (y; y < opt.length; y++) {
+      opt[y].parentNode.value = opt[y].value;
+    }
+
+    //# Remove the button
+    document.body.removeChild(this);
+  }
+
+  function on() {
+    var button = buttonCreate();
+    button.addEventListener('click', whenClicked, false);
+  }
+
+  return {
+    customOptions: customOptions,
+    on: on
+  };
+
+}());
